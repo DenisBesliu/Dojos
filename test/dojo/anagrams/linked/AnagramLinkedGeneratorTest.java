419ee4ab18468464a578afbe795718f7a6b8d9e8
stkiller <golovorub@gmail.com>
2013-01-23 11:38:11 +0200

    Added build script to run the tests

--- test/dojo/anagrams/linked/AnagramLinkedGeneratorTest.java
+++ test/dojo/anagrams/linked/AnagramLinkedGeneratorTest.java
@@ -1,10 +1,12 @@
 package dojo.anagrams.linked;
 
 import dojo.anagrams.recursive.AnagramRecursiveGeneratorTest;
+import org.junit.Ignore;
 
 /**
  * @author apodoprigora (23/01/2013)
  */
+@Ignore
 public class AnagramLinkedGeneratorTest extends AnagramRecursiveGeneratorTest {
 
     @Override
