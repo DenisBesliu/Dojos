0e553da90fb2052cbdbc24c38c2ddb7e63ec871c
stkiller <golovorub@gmail.com>
2013-01-23 16:04:10 +0200

    Improved Tests

--- src/dojo/anagrams/linked/AnagramLinkedGenerator.java
+++ src/dojo/anagrams/linked/AnagramLinkedGenerator.java
@@ -1,5 +1,6 @@
 package dojo.anagrams.linked;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,44 @@ import dojo.anagrams.AnagramGenerator;
 public class AnagramLinkedGenerator implements AnagramGenerator{
 
     public List<String> generate(final String aValue) {
-        return Collections.emptyList();
+        if (aValue == null || aValue.isEmpty()) {
+            return Collections.emptyList();
+        }
+        if (aValue.length() == 1) {
+            return Collections.singletonList(aValue);
+        }
+        final List<String> result = new ArrayList<String>();
+        final int wordLength = aValue.length();
+        final List<Character> list = createLinkedCharacters(aValue);
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 1; j < list.size(); j++) {
+                final StringBuilder currentWord = new StringBuilder();
+                int currentPosition = i;
+                if (list.get(currentPosition % list.size()) == null) {
+                    continue;
+                }
+                while (currentWord.length() < wordLength) {
+                    final Character currentChar = list.get(currentPosition % list.size());
+                    if (currentChar != '_') {
+                        currentWord.append(currentChar);
+                    }
+                    currentPosition += j;
+                }
+                result.add(currentWord.toString());
+            }
+        }
+        return result;
+    }
+
+
+    private List<Character> createLinkedCharacters(final String aValue) {
+        final List<Character> list = new ArrayList<Character>();
+        for (int i = 0; i < aValue.length(); i++) {
+            list.add(aValue.charAt(i));
+        }
+        if (list.size() % 2 == 0) {
+            list.add('_');
+        }
+        return list;
     }
 }
--- test/dojo/anagrams/recursive/AnagramRecursiveGeneratorTest.java
+++ test/dojo/anagrams/recursive/AnagramRecursiveGeneratorTest.java
@@ -1,6 +1,9 @@
 package dojo.anagrams.recursive;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import dojo.anagrams.AnagramGenerator;
 import static org.junit.Assert.assertEquals;
@@ -46,59 +49,59 @@ public class AnagramRecursiveGeneratorTest {
     @Test
     public void oneCharValue() {
         final List<String> result = anagramGenerator.generate("a");
-        assertTrue(result.contains("a"));
-        assertEquals(1, result.size());
+        assertVarArgs(result, "a");
     }
 
 
     @Test
     public void twoCharValue() {
         final List<String> result = anagramGenerator.generate("ab");
-        assertEquals(2, result.size());
-        assertTrue(result.contains("ab"));
-        assertTrue(result.contains("ba"));
+        assertVarArgs(result, "ab", "ba");
     }
 
 
     @Test
     public void threeCharValue() {
         final List<String> result = anagramGenerator.generate("abc");
-        assertEquals(6, result.size());
-        assertTrue(result.contains("abc"));
-        assertTrue(result.contains("acb"));
-        assertTrue(result.contains("bca"));
-        assertTrue(result.contains("bac"));
-        assertTrue(result.contains("cba"));
-        assertTrue(result.contains("cab"));
+        assertVarArgs(result, "abc", "acb", "bca", "bac", "cba", "cab");
     }
 
 
     @Test
     public void fourCharValue() {
         final List<String> result = anagramGenerator.generate("abcd");
-        assertEquals(24, result.size());
-        System.out.println(result);
-        /*
-         * assertVarArgs(result, new String[]{"abcd", "abdc", "acbd", "acdb", "adbc", "adcb, bacd, badc, bcad, bcda, bdac, bdca, cabd,
-         * cadb",
-         */
-        // "cbad",
-        // "cbda, cdab, cdba, dabc, dacb, dbac, dbca, dcab, dcba});
-    }
-
-
-    private void assertVarArgs(final List<String> aResult, final String[] aExpected) {
-        for (final String value : aExpected) {
-            assertTrue(aResult.contains(value));
-        }
+        assertVarArgs(result, "abcd", "abdc", "acbd", "acdb", "adbc", "adcb", "bacd", "badc", "bcad", "bcda", "bdac", "bdca", "cabd",
+                      "cadb", "cbad", "cbda", "cdab", "cdba", "dabc", "dacb", "dbac", "dbca", "dcab", "dcba");
     }
 
 
     @Test
     public void biroCharValue() {
         final List<String> result = anagramGenerator.generate("biro");
-        assertEquals(24, result.size());
-        System.out.println(result);
+        assertVarArgs(result, "biro", "bior", "brio", "broi", "boir", "bori", "ibro", "ibor", "irbo", "irob", "iobr", "iorb", "rbio",
+                      "rboi", "ribo", "riob", "roib", "robi", "obir", "obri", "oibr", "oirb", "orbi", "orib");
+    }
+
+
+    private void assertVarArgs(final List<String> aActual, final String... aExpected) {
+        assertTrue("Expected result should be unique", areUnique(Arrays.asList(aExpected)));
+        assertTrue("Actual result should be unique",areUnique(aActual));
+
+        System.out.println("Expected : " + Arrays.toString(aExpected));
+        System.out.println("Actual   : " + aActual);
+        assertEquals("The sizes should match", aExpected.length, aActual.size());
+        for (final String value : aExpected) {
+            assertTrue("Actual should contain : " +value ,aActual.contains(value));
+        }
+    }
+
+
+    private boolean areUnique(final Iterable<String> aActual) {
+        final Set<String> temp = new HashSet<String>();
+        for (final String string : aActual) {
+            temp.add(string);
+        }
+        return temp.size() == temp.size();
     }
 
 }
